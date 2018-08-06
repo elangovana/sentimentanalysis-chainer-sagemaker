6d227f5c53f6eb4bd0b344e74e9f68b90aa115f5
aeg <aeg@amazon.com>
2018-08-06 21:04:20 +1000

    Add mechanims to split dataset

--- custom_chainer/dataprep/splitter.py
+++ custom_chainer/dataprep/splitter.py
@@ -5,20 +5,87 @@ import chainer
 import os
 
 import dataprep.YelpChainerDataset
+import logging
+
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+logger = logging.getLogger(__name__)
 
 
-def split_traintest(file, first_size_fraction=.8, seed=1572) -> tuple((chainer.datasets.sub_dataset, chainer.datasets.sub_dataset)):
+def split_traintest(file_or_dir, first_size_fraction=.8, seed=1572) -> tuple(
+    (chainer.datasets.sub_dataset, chainer.datasets.sub_dataset)):
     """
 Splits a file into 2  sets such as training & test.
-    :param file: The file to split
+    :param file_or_dir: The file to split
     :param first_size_fraction: The fraction of data to be polaced in the first set. Say if this value is .7, then 70% os the data is placed in the first set
     :param seed: The random seed to fix
     :return: 2 datasets
     """
-    dataset = dataprep.YelpChainerDataset.YelpChainerDataset(file, delimiter=",", encoding="utf-8", quote_charcter='"')
-    first_size= int(len(dataset) * first_size_fraction)
+    if os.path.isfile(file_or_dir):
+        dataset = dataprep.YelpChainerDataset.YelpChainerDataset(file_or_dir, delimiter=",", encoding="utf-8",
+                                                                 quote_charcter='"')
+    else:
+        dataset = chainer.datasets.ConcatenatedDataset(get_dataset_array(file_or_dir))
+
+    first_size = int(len(dataset) * first_size_fraction)
     first, second = chainer.datasets.split_dataset_random(dataset, first_size, seed=seed)
-    return first,second
+    return first, second
+
+
+def get_dataset_array(base_dir):
+    # Can make this dynamic, but in this sample 2 parts of the file
+    datasets = []
+    for f in os.listdir(base_dir):
+        datasets.append(
+            dataprep.YelpChainerDataset.YelpChainerDataset(f, delimiter=",", encoding="utf-8", quote_charcter='"'))
+
+    return datasets
+
+
+def split(filepath, outputdir, has_header=True, no_of_parts=None, delimiter=",", encoding="utf-8", quote_character='"'):
+    # Approximate each line is 2KB
+    KB = 1024
+    approx_size_of_each_line = 1 * KB
+    approx_total_lines = os.path.getsize(filepath) / approx_size_of_each_line
+
+    # Get number of lines per part
+    MB = 2 * (KB * KB)
+    no_of_parts = no_of_parts or int(os.path.getsize(filepath) / MB) + 1
+    no_lines_per_part = int(approx_total_lines / no_of_parts)
+
+    logger.info("Dividing file {} into estimated {} parts".format(filepath, no_of_parts))
+
+    with open(filepath, encoding=encoding) as handle:
+        csv_reader = csv.reader(handle, delimiter=delimiter, quotechar=quote_character)
+        # Skip first line if header
+        header = ""
+        if has_header:
+            header = next(csv_reader)
+
+        # Count the number of lines
+        part_index = 0
+        end_of_file = False
+
+        while (not end_of_file):
+
+            part_index = part_index + 1
+            part_name = "{}_part_{:03d}.csv".format(os.path.basename(filepath), part_index)
+            output_part = os.path.join(outputdir, part_name)
+
+            with open(output_part, "w") as handle:
+                csv_writer = csv.writer(handle, delimiter=delimiter, quotechar=quote_character)
+                if has_header:
+                    csv_writer.writerow(header)
+
+                for i in range(0, no_lines_per_part):
+                    try:
+                        line = next(csv_reader)
+                        csv_writer.writerow(line)
+                    except StopIteration:
+                        end_of_file = True
+                        break
+                logger.info("Completed part {}".format(output_part))
+        logger.info("Completed dividing files sucessfully")
 
 
 def write_csv(outputfile, dataset):
@@ -28,8 +95,7 @@ def write_csv(outputfile, dataset):
             csv_writer.writerow(l)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("inputfile",
                         help="The input file, yelpreviews.csv to split")
@@ -38,11 +104,22 @@ if __name__=="__main__":
                         help="The output directory")
 
     parser.add_argument("--first-file-name",
-                        help="The output directory", default= "train.shuffled.csv")
+                        help="The output directory", default="train.shuffled.csv")
     parser.add_argument("--second-file-name",
-                        help="The output directory", default= "test.shuffled.csv")
+                        help="The output directory", default="test.shuffled.csv")
+
+    parser.add_argument("--divide",
+                        help="Whether to divide, Enter Y or N. If you choose Y, then --first-file-name and --second-file-name are ignored ",
+                        default="N", choices={'Y', 'N'})
     args = parser.parse_args()
     print(args.__dict__)
-    first, second = split_traintest(args.inputfile)
-    write_csv(os.path.join(args.outdir, "train.shuffled.csv"), first)
-    write_csv(os.path.join(args.outdir, "test.shuffled.csv"), second)
+
+    logger.info("Starting ... ")
+    if args.divide == "Y":
+        split(args.inputfile, args.outdir)
+    else:
+        first, second = split_traintest(args.inputfile)
+        write_csv(os.path.join(args.outdir, "train.shuffled.csv"), first)
+        write_csv(os.path.join(args.outdir, "test.shuffled.csv"), second)
+
+    logger.info("Completed ... ")
