e94791c06d69b39ab61db8beebd75a211efd1f52
aeg <aeg@amazon.com>
2018-08-06 17:42:59 +1000

    Move bag of words encoder into separate file

---
+++ custom_chainer/BOWEncoder.py
@@ -0,0 +1,35 @@
+import chainer
+import numpy
+from chainer import links as L, functions as F
+
+from nets import embed_init, block_embed
+
+
+class BOWEncoder(chainer.Chain):
+
+    """A BoW encoder with word embedding.
+
+    This model encodes a sentence as just a set of words by averaging.
+
+    Args:
+        n_vocab (int): The size of vocabulary.
+        n_units (int): The number of units of word embedding.
+        dropout (float): The dropout ratio.
+
+    """
+
+    def __init__(self, n_vocab, n_units, dropout=0.1):
+        super(BOWEncoder, self).__init__()
+        with self.init_scope():
+            self.embed = L.EmbedID(n_vocab, n_units, ignore_label=-1,
+                                   initialW=embed_init)
+
+        self.out_units = n_units
+        self.dropout = dropout
+
+    def __call__(self, xs):
+        x_block = chainer.dataset.convert.concat_examples(xs, padding=-1)
+        ex_block = block_embed(self.embed, x_block)
+        x_len = self.xp.array([len(x) for x in xs], numpy.int32)[:, None, None]
+        h = F.sum(ex_block, axis=2) / x_len
+        return h
--- custom_chainer/nets.py
+++ custom_chainer/nets.py
@@ -5,6 +5,8 @@ import chainer.functions as F
 import chainer.links as L
 from chainer import reporter
 
+from BOWEncoder import BOWEncoder
+
 embed_init = chainer.initializers.Uniform(.25)
 
 
@@ -213,36 +215,6 @@ class MLP(chainer.ChainList):
             x = F.dropout(x, ratio=self.dropout)
             x = F.relu(link(x))
         return x
-
-
-class BOWEncoder(chainer.Chain):
-
-    """A BoW encoder with word embedding.
-
-    This model encodes a sentence as just a set of words by averaging.
-
-    Args:
-        n_vocab (int): The size of vocabulary.
-        n_units (int): The number of units of word embedding.
-        dropout (float): The dropout ratio.
-
-    """
-
-    def __init__(self, n_vocab, n_units, dropout=0.1):
-        super(BOWEncoder, self).__init__()
-        with self.init_scope():
-            self.embed = L.EmbedID(n_vocab, n_units, ignore_label=-1,
-                                   initialW=embed_init)
-
-        self.out_units = n_units
-        self.dropout = dropout
-
-    def __call__(self, xs):
-        x_block = chainer.dataset.convert.concat_examples(xs, padding=-1)
-        ex_block = block_embed(self.embed, x_block)
-        x_len = self.xp.array([len(x) for x in xs], numpy.int32)[:, None, None]
-        h = F.sum(ex_block, axis=2) / x_len
-        return h
 
 
 class BOWMLPEncoder(chainer.Chain):
