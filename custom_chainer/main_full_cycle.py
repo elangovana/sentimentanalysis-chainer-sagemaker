934318d70a18e0d0bd255f7532e839bc2e8f889d
aeg <aeg@amazon.com>
2018-08-11 15:57:56 +1000

    Add main full cycle

--- custom_chainer/main_full_cycle.py
+++ custom_chainer/main_full_cycle.py
@@ -2,31 +2,34 @@ import argparse
 import json
 import os
 import logging
+
+import sys
+
 from predict import get_model, get_formatted_input, predict, \
     get_formatted_output
 from train import run_train
 
 
-
 def train():
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
 
     parser = argparse.ArgumentParser(
         description='Chainer example: Text Classification')
     parser.add_argument('--traindata', required=True,
                         help='The dataset file with respect to the train directory')
     parser.add_argument('--traindata-dir',
-                        help='The directory containing training artifacts such as training data', default=os.environ.get('SM_CHANNEL_TRAIN', "."))
+                        help='The directory containing training artifacts such as training data',
+                        default=os.environ.get('SM_CHANNEL_TRAIN', "."))
     parser.add_argument('--testdata', '-td',
                         help='The test dataset file with respect to the test directory', default=None)
     parser.add_argument('--testdata-dir', '-tdir',
-                        help='The directory containing test artifacts such as test data', default=os.environ.get('SM_CHANNEL_TEST', "."))
+                        help='The directory containing test artifacts such as test data',
+                        default=os.environ.get('SM_CHANNEL_TEST', "."))
     parser.add_argument('--batchsize', '-b', type=int, default=64,
                         help='Number of images in each mini-batch')
     parser.add_argument('--epoch', '-e', type=int, default=30,
                         help='Number of sweeps over the dataset to train')
-    parser.add_argument('--gpu', '-g', type=int, default=int(os.environ.get('SM_NUM_GPUS', 0))-1,
+    parser.add_argument('--gpu', '-g', type=int, default=int(os.environ.get('SM_NUM_GPUS', 0)) - 1,
                         help='GPU ID (negative value indicates CPU)')
     parser.add_argument('--out', '-o', default=os.environ.get('SM_OUTPUT_DATA_DIR', "result_data"),
                         help='Directory to output the result')
@@ -44,16 +47,13 @@ def train():
 
     args = parser.parse_args()
 
+    logger.info("Invoking training with arguments \n ..{}".format(json.dumps(args.__dict__, indent=2)))
 
-    logger.info("Invoking training with arguments \n ..",json.dumps(args.__dict__, indent=2))
-
-
-    #args parse
-
+    # args parse
 
     batchsize = args.batchsize
     model = args.model
-    training_set= os.path.join(args.traindata_dir, args.traindata)
+    training_set = os.path.join(args.traindata_dir, args.traindata)
     validation_set = os.path.join(args.testdata_dir, args.testdata) if args.testdata is not None else None
     dataset = [training_set, validation_set] if validation_set is not None else [training_set]
     char_based = args.char_based
@@ -64,7 +64,7 @@ def train():
     epoch = args.epoch
     out = args.out
 
-    run_train(batchsize, char_based,  dataset, dropout, epoch, gpu, model, no_layers, out,
+    run_train(batchsize, char_based, dataset, dropout, epoch, gpu, model, no_layers, out,
               unit)
 
 
@@ -77,8 +77,7 @@ def model_fn(model_dir):
                 model_full_path = root
                 break
 
-    return get_model(model_full_path,os.environ.get('SM_NUM_GPUS', 0) - 1 )
-
+    return get_model(model_full_path, os.environ.get('SM_NUM_GPUS', 0) - 1)
 
 
 def input_fn(request_body, request_content_type):
@@ -87,24 +86,19 @@ def input_fn(request_body, request_content_type):
     return get_formatted_input(request_body, request_content_type)
 
 
-
 def predict_fn(input_object, model):
-
-    return predict(input_object, model, os.environ.get('SM_NUM_GPUS', 0)-1)
+    return predict(input_object, model, os.environ.get('SM_NUM_GPUS', 0) - 1)
 
 
 # Serialize the prediction result into the desired response content type
 def output_fn(prediction, response_content_type):
     logger = logging.getLogger(__name__)
     logger.debug("Calling output for content_type ..{}".format(response_content_type))
-    result =    get_formatted_output(prediction, response_content_type)
+    result = get_formatted_output(prediction, response_content_type)
     logger.debug("Received output {} ..".format(result))
     return result
 
 
 if __name__ == '__main__':
-    # TODO: Check why logging doesnt work on SageMaker
-    logging.basicConfig(level=logging.DEBUG, handlers=[
-        logging.StreamHandler()])
-
+    logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)])
     train()
