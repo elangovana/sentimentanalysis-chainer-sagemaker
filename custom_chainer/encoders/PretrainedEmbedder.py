bf40cfd065f523e72b848b24ae513a82103f50ff
aeg <aeg@amazon.com>
2018-08-27 13:50:27 +1000

    fix comments

--- custom_chainer/encoders/PretrainedEmbedder.py
+++ custom_chainer/encoders/PretrainedEmbedder.py
@@ -20,7 +20,7 @@ class PretrainedEmbedder:
     @staticmethod
     def load(handle, other_words_embed_dict):
         """
-Expects the stream to string to contain embedding in space separated format with the first column containing the word itself.
+Expects the stream of strings to contain word embedding. Each record must be in space separated format with the first column containing the word itself.
 Each record is separated by new lines
 e.g for an embedding of size 10
 zsombor -0.75898 -0.47426 0.4737 0.7725 -0.78064 0.23233 0.046114 0.84014 0.243710 .022978
@@ -40,7 +40,7 @@ sandberger 0.072617 -0.51393 0.4728 -0.52202 -0.35534 0.34629 0.23211 0.23096 0.
             word_index_dict[word] = len(word_index_dict)
             embeddings_array.append(embeddings)
 
-        # Add random embeddings for additional words that do not exist in handle
+        # Add  embeddings for additional words that do not exist in handle
         for w in other_words_embed_dict.keys():
             if word_index_dict.get(w, None) is None:
                 word_index_dict[w] = len(word_index_dict)
