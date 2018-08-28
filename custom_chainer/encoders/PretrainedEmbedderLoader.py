bc62d269cd33a3f9a949d8941386cf8b4eee8c77
aeg <aeg@amazon.com>
2018-08-28 18:02:29 +1000

    Separate loader and embedder

---
+++ custom_chainer/encoders/PretrainedEmbedderLoader.py
@@ -0,0 +1,41 @@
+import logging
+
+import chainer
+import numpy as np
+from chainer.backends import cuda
+
+
+class PretrainedEmbedderLoader:
+
+
+    def __call__(self, handle, other_words_embed_dict):
+        """
+Expects the stream of strings to contain word embedding. Each record must be in space separated format with the first column containing the word itself.
+Each record is separated by new lines
+e.g for an embedding of size 10
+zsombor -0.75898 -0.47426 0.4737 0.7725 -0.78064 0.23233 0.046114 0.84014 0.243710 .022978
+sandberger 0.072617 -0.51393 0.4728 -0.52202 -0.35534 0.34629 0.23211 0.23096 0.26694 .41028
+        :param handle: handle containing the embedding
+        """
+        word_index_dict = {}
+        embeddings_array = []
+        other_words_embed_dict = other_words_embed_dict or {}
+        xp = cuda.get_array_module()
+
+        # Load embeddings from file
+        for line in handle:
+            values = line.split()
+            word = values[0]
+            embeddings = xp.asarray(values[1:], dtype='float32')
+            word_index_dict[word] = len(word_index_dict)
+            embeddings_array.append(embeddings)
+
+        # Add  embeddings for additional words that do not exist in handle
+        for w in other_words_embed_dict.keys():
+            if word_index_dict.get(w, None) is None:
+                word_index_dict[w] = len(word_index_dict)
+                embeddings_array.append(xp.asarray(other_words_embed_dict[w], dtype='float32'))
+
+        # Convert to ndarray or cupy
+        embeddings_array = xp.asarray(embeddings_array, dtype='float32')
+        return word_index_dict, embeddings_array
--- custom_chainer/tests/test_pretrainedEmbedderLoader.py
+++ custom_chainer/tests/test_pretrainedEmbedderLoader.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 import numpy as np
 
 from encoders.PretrainedEmbedder import PretrainedEmbedder
+from encoders.PretrainedEmbedderLoader import PretrainedEmbedderLoader
 
 
 class TestPretrainedEmbedder(TestCase):
@@ -16,16 +17,16 @@ class TestPretrainedEmbedder(TestCase):
         other_words_embed = None
         emdedding = np.random.randint(0, 100, size=(no_words, embed_len))
         get_mock_embed_handle = self.get_mock_embedding_handle(words_array, emdedding)
-        actual = np.random.randint(0, 100, size=(no_words, embed_len))
 
         # Act
-        word_index, weights = PretrainedEmbedder.load(handle=get_mock_embed_handle,
-                                                      other_words_embed_dict=other_words_embed)
-        sut = PretrainedEmbedder(word_index, weights)
-        sut(actual)
+        sut = PretrainedEmbedderLoader()
+        actual_word_index, actual_weights = sut(handle=get_mock_embed_handle,
+                                                other_words_embed_dict=other_words_embed)
 
         # Assert
-        self.assertListEqual(actual.tolist()[0:no_words], emdedding.tolist())
+        for w, e in zip(words_array, emdedding):
+            index = actual_word_index[w]
+            self.assertSequenceEqual(actual_weights[index].tolist(), e.tolist())
 
     def test_should_return_embeddings_unknown(self):
         ## Arrange
@@ -38,24 +39,24 @@ class TestPretrainedEmbedder(TestCase):
         actual = np.random.randint(0, 100, size=(no_words * 2, embed_len))
 
         # Act
-        word_index, weights = PretrainedEmbedder.load(handle=get_mock_embed_handle, other_words_embed_dict= other_words_embed)
-        sut = PretrainedEmbedder(word_index, weights)
-        sut(actual)
+        sut = PretrainedEmbedderLoader()
+        actual_word_index, actual_weights = sut(handle=get_mock_embed_handle, other_words_embed_dict=other_words_embed)
 
         # Assert
-        expected = [[0] for i in range(0, no_words)]
-        for w in other_words_embed.keys():
-            i = sut.word_index[w] - no_words
-            expected[i]=other_words_embed[w].tolist()
-        self.assertListEqual(actual.tolist()[no_words:2*no_words], expected)
+        # Assert0
+        for w, e in other_words_embed.items():
+            index = actual_word_index[w]
+            self.assertSequenceEqual(actual_weights[index].tolist(), e.tolist())
 
     def get_other_words_embed(self, embed_len, no_words):
         # Other word embedding
         other_words_embed = {}
+
         for r in range(0, no_words):
             w = "other_word_{}".format(r)
+
             other_words_embed[w] = np.random.randint(0, 100, size=embed_len)
-        return other_words_embed
+        return  other_words_embed
 
     def get_mock_embedding_handle(self, words, embed):
         stringIO = StringIO()
